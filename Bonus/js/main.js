5c7e1d322efd17b93a259760b89846d65124f00c
19nikilangiu91 <niki.langiu@gmail.com>
2022-12-06 16:52:34 +0100

    Upgrade Bonus

--- Bonus/js/main.js
+++ Bonus/js/main.js
@@ -1,9 +1,13 @@
+// Bonus
+// Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
 const { createApp } = Vue
 
   createApp({
     data() {
       return {
         title: "Email's list",
+        emailsNeeded: 10,
         emails: [],
       }
     },
