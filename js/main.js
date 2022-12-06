355f58458046a830f30b1b6e1b336e1d95d9c394
19nikilangiu91 <niki.langiu@gmail.com>
2022-12-06 15:28:56 +0100

    Prova stampa singola email random

--- js/main.js
+++ js/main.js
@@ -3,6 +3,17 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
+      title: "Email's list",
+      emails: [],
     }
+  },
+
+  mounted(){
+    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+    .then((result)=>{
+      console.log(result.data.response);
+      this.emails.push(result.data.response);
+    });
   }
+
 }).mount('#myApp')
